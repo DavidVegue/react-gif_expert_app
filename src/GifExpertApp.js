6ed110351c9c760ef0b97aaadd62dd65b55a0d06
Vegue <david.vegue@gmail.com>
2021-05-20 19:10:57 +0200

    Con boton de add category

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () => {
+        //Añadir una nueva categoria
+        const newName = 'Pikachu';
+        if (!categories.includes(newName)) {
+            setCategories( [...categories, newName] );
+        }
+    };
 
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
+
+            <button onClick = {handleAdd} >Agregar</button>
+
+            <ol>
+                { 
+                    categories.map( category => {
+                        return <li key = {category}>{category}</li>;
+                    })
+                }
+            </ol>
+
         </>
-    )
+    );
 
 };
 
